ccfacd8ee47d0d7eed00bce54d4b8c9b88aef1a4
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-03-31 22:10:42 +0300

    Завдання 2 (оновлення)

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -77,25 +77,20 @@ function addLeadingZero(value) {
 }
 
 function convertMs(ms) {
-  // Кількість мілісекунд
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // днів
-  const days = addLeadingZero(Math.floor(ms / day));
-
-  // годин
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-
-  // хвилин
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-
-  // секунд
-  const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
